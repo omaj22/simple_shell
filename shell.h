a3c7932882999dca5f1a65b467397afc30f72287
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-07-26 13:30:57 -0700

    test files

--- shell.h
+++ shell.h
@@ -1,41 +1,70 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#ifndef SHELL_H
+#define SHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <string.h>
 #include <sys/wait.h>
-#include <fcntl.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stddef.h>
+#include <sys/stat.h>
 #include <signal.h>
-#include <dirent.h>
-#include <glob.h>
 
-#define MAX_INPUT_LENGTH 1024
-#define MAX_ARGS 64
-#define MAX_HISTORY 100
-#define MAX_ALIASES 10
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
+char *_strdup(char *str);
+char *concat_all(char *name, char *sep, char *value);
 
-char *history[MAX_HISTORY];
-/*int history_count = 0;*/
-char *aliases[MAX_ALIASES * 2];
+char **splitstring(char *str, const char *delim);
+void execute(char **argv);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-char *read_input(void);
-char **tokenize_input(char *input);
-int execute_command(char **args);
-int handle_builtin(char **args);
-void handle_background(char **args);
-void add_to_history(char *input);
-void display_history();
-void handle_signal(int signal);
-void initialize_shell();
-char *get_command_with_tab_completion(char *input);
-void handle_aliases(char **args);
-void run_script(const char *script_path);
-int check_for_wildcards(char **args);
-void manage_background_processes();
-void display_prompt();
-void load_configuration();
+
+extern char **environ;
+
+/**
+ * struct list_path - Linked list containing PATH directories
+ * @dir: directory in path
+ * @p: pointer to next node
+ */
+typedef struct list_path
+{
+	char *dir;
+	struct list_path *p;
+} list_path;
+
+
+char *_getenv(const char *name);
+list_path *add_node_end(list_path **head, char *str);
+list_path *linkpath(char *path);
+char *_which(char *filename, list_path *head);
+
+/**
+ * struct mybuild - pointer to function with corresponding buildin command
+ * @name: buildin command
+ * @func: execute the buildin command
+ */
+typedef struct mybuild
+{
+	char *name;
+	void (*func)(char **);
+} mybuild;
+
+void(*checkbuild(char **arv))(char **arv);
+int _atoi(char *s);
+void exitt(char **arv);
+void env(char **arv);
+void _setenv(char **arv);
+void _unsetenv(char **arv);
+
+void freearv(char **arv);
+void free_list(list_path *head);
+
 
 #endif
