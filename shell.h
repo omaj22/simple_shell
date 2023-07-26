1249e57ddf2598dee090c44ea7e6f3f861b1b4c1
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-07-26 10:07:13 -0700

    updated the header file

--- shell.h
+++ shell.h
@@ -7,13 +7,31 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <dirent.h>
+#include <glob.h>
 
 #define MAX_INPUT_LENGTH 1024
 #define MAX_ARGS 64
+#define MAX_HISTORY 100
+#define MAX_ALIASES 10
 
-char* read_input(void);
-char** tokenize_input(char* input);
+char *read_input(void);
+char **tokenize_input(char *input);
 int execute_command(char **args);
 int handle_builtin(char **args);
+void handle_background(char **args);
+void add_to_history(char *input);
+void display_history();
+void handle_signal(int signal);
+void initialize_shell();
+char *get_command_with_tab_completion(char *input);
+void handle_aliases(char **args);
+void run_script(const char *script_path);
+int check_for_wildcards(char **args);
+void manage_background_processes();
+void display_prompt();
+void load_configuration();
 
 #endif
