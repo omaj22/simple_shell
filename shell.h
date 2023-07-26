3a8716ce68f1f621e4539169096f832d8733e5b1
Kleinbooi Mathaila <phaswanemathaila@gmail.com>
2023-07-26 15:16:43 -0700

    header file

--- shell.h
+++ shell.h
@@ -1,7 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-/* header files */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,31 +12,28 @@
 #include <fcntl.h>
 #include <errno.h>
 
-/* Global variable */
 extern char **environ;
 
-/* Macros */
 #define BUFSIZE 256
 #define TOKENSIZE 64
-#define PRINT(c) (write(STDOUT_FILENO, c, _strlen(c)))
+#define PRINT(c) (write(STDOUT_FILENO, c, _stringLength(c)))
 #define PROMPT "$ "
 #define SUCCESS (1)
 #define FAIL (-1)
 #define NEUTRAL (0)
 
-/* Struct */
 
 /**
- * struct sh_data - Global data structure
- * @line: the line input
- * @args: the arguments token
+ * struct sh_data - Global
+ * @line: line input
+ * @args: arguments token
  * @error_msg: the global path
- * @cmd: the parsed command
- * @index: the command index
  * @oldpwd: the old path visited
  * @env: the environnment
+ * @cmd: parsed command
+ * @index: command index
  *
- * Description: A structure contains all the variables needed to manage
+ * Description: structure contains all the variables needed to manage
  * the program, memory and accessability
  */
 typedef struct sh_data
@@ -49,66 +45,61 @@ typedef struct sh_data
 	char *oldpwd;
 	unsigned long int index;
 	char *env;
-} sh_t;
+} sh;
+
 /**
- * struct builtin - Manage the builtin functions
- * @cmd: the command line on string form
- * @f: the associated function
+ * struct builtin - Represents a built-in command and its associated function
+ * @cmd: The command name as a string
+ * @f: A pointer to the associated function for the built-in 
  *
  * Description: this struct made to manage builtins cmd
  */
 typedef struct builtin
 {
 	char *cmd;
-	int (*f)(sh_t *data);
+	int (*f)(sh *data);
 } blt_t;
-/* ----------Process prototype------------*/
-int read_line(sh_t *);
-int split_line(sh_t *);
-int parse_line(sh_t *);
-int process_cmd(sh_t *);
 
-/* ----------String prototype------------*/
-char *_strdup(char *str);
-char *_strcat(char *first, char *second);
-int _strlen(char *str);
-char *_strchr(char *str, char c);
-int _strcmp(char *s1, char *s2);
+int line_reader(sh *);
+int line_spliter(sh *);
+int line_parser(sh *);
+int cmd_processor(sh *);
 
-/* ----------More String prototype-------*/
-char *_strcpy(char *dest, char *source);
+int _isPathFormat(sh *data);
+void _convertToShortForm(sh *data);
+int is_builtin(sh *data);
 
-/* ----------Memory prototype------------*/
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *_memset(char *s, char byt, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int free_data(sh_t *);
 
-/* ----------Tools prototype-------------*/
+char *_duplicateString(char *str);
+char *_concatenateStrings(char *first, char *second);
+int _stringLength(char *str);
+char *_findFirstOccurrence(char *str, char c);
+int _compareStrings(char *s1, char *s2);
+
+char *_copyString(char *dest, char *source);
+
+void *_reallocateMemory(void *ptr, unsigned int old_size, unsigned int new_size);
+char *_setMemory(char *s, char byt, unsigned int n);
+char *_copyMemory(char *dest, char *src, unsigned int n);
+int _freeData(sh *);
+
 void *fill_an_array(void *a, int el, unsigned int len);
-void signal_handler(int signo);
-char *_getenv(char *path_name);
-void index_cmd(sh_t *data);
-void array_rev(char *arr, int len);
+void _handleSignal(int signo);
+char *_getEnvironmentVariable(char *path_name);
+void _performIndexCommand(sh *data);
+void _reverseArray(char *arr, int len);
 
-/* ----------More tools prototype--------*/
-char *_itoa(unsigned int n);
-int intlen(int num);
-int _atoi(char *c);
-int print_error(sh_t *data);
-int write_history(sh_t *data);
-int _isalpha(int c);
+char *_unsignedIntToString(unsigned int n);
+int _integerLength(int num);
+int _stringToInteger(char *c);
+int _printerror(sh *data);
+int write_history(sh *data);
+int _isAlphabetic(int c);
 
-/* -------------Builtins-----------------*/
-int abort_prg(sh_t *data);
-int change_dir(sh_t *data);
-int display_help(sh_t *data);
-int handle_builtin(sh_t *data);
-int check_builtin(sh_t *data);
+int _abortProgram(sh *data);
+int _changeDirectory(sh *data);
+int _displayHelpMessage(sh *data);
+int _handleBuiltInCommand(sh *data);
+int _isBuiltInCommand(sh *data);
 
-/* -------------Parse-----------------*/
-int is_path_form(sh_t *data);
-void is_short_form(sh_t *data);
-int is_builtin(sh_t *data);
-
-#endif /* SHELL_H */
+#endif 
